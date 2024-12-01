5b6eb15a3582acc27c3e6c3db4bf23b0e840d548
Joshua Donaldson <donaldson.josh.96@gmail.com>
2024-12-01 14:58:36 -0800

    add control options

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from time import sleep
 import pandas as pd
+import warnings
 
-from gui.sidebar import mk_sidebar, alert_user
+from gui.sidebar import mk_sidebar, alert_user, BOLUS_TIMES, DEADBAND
 from gui.instructions import render_instructions
 from gui.metrics import mk_metrics
 from gui.graph import write_XP_graph, write_nutrients_graph, write_volume_graph
@@ -17,13 +18,35 @@ from millify import millify
 # profiler = Profiler()
 # profiler.start()
 
+# App Configuration
+
 st.set_page_config(page_title="BioReactorSim", page_icon=":joystick:", layout="wide")
+
+warnings.filterwarnings(
+    "ignore", message="I don't know how to infer vegalite type from 'empty'"
+)
+
+# Constants
+
+T_MAX = 250
+N = 25
+
+# Some initial values
+
+GLC_F = 2500
+Glc_F = GLC_F  # mM
+Gln_F = 100  # mM
+F_Glc = 0  # L / h
+F_Gln = 0  # L / h
+F_B = 0  # L / h
 
 if not "auto_refresh" in st.session_state:
     st.session_state.auto_refresh = False
 
 if not "sleep_time" in st.session_state:
     st.session_state.sleep_time = 0.1
+
+# Create Sidebar
 
 with st.sidebar:
     mk_sidebar(
@@ -34,8 +57,7 @@ with st.sidebar:
 # initialize during first run
 if "data" not in st.session_state and st.session_state.auto_refresh:
     # Integration parameters
-    t_max = 250  # h
-    N = 25  # points
+    t_max = T_MAX  # h
 
     [Glc_0, Gln_0, X_0, P_0, Amm, Lac, V_0] = st.session_state.initial_vals
 
@@ -53,13 +75,8 @@ if "data" not in st.session_state and st.session_state.auto_refresh:
         pd.DataFrame([res]).astype("float64").set_index("t", drop=False)
     )
 
-# Some initial values
-Glc_F = 2500  # mM
-Gln_F = 100
-F_Glc = 0
-F_Gln = 0
-F_B = 0
 
+# Title
 st.title("CHO Cell Bioreactor Simulator")
 st.subheader("An interactive bioprocess simulation")
 
@@ -119,18 +136,46 @@ with tab2:
 if st.session_state.auto_refresh:
     data = st.session_state["data"]
 
-    t0_glc, tn_glc = st.session_state.t_glc
-    if t0_glc <= data["t"].iloc[-1] <= tn_glc:
-        F_Glc = st.session_state.flows[0]
-    else:
-        F_Glc = 0
+    # Get Control Values from Front-end
+    if st.session_state.control_mode == "Bolus Feed":
 
-    t0_gln, tn_gln = st.session_state.t_gln
-    if t0_gln <= data["t"].iloc[-1] <= tn_gln:
-        F_Gln = st.session_state.flows[1]
-    else:
-        F_Gln = 0
+        app_dt = st.session_state.app_dt
+        for i, _ in enumerate(BOLUS_TIMES):
+            if data["t"].iloc[-1] <= BOLUS_TIMES[i] < data["t"].iloc[-1] + app_dt:
+                F_Glc = st.session_state.bolus_feeds[i] / 1000
+                Glc_F = GLC_F * 10
+                break
+            else:
+                F_Glc = 0
+                Glc_F = GLC_F
 
+    elif st.session_state.control_mode == "Bang Control":
+
+        Glc_DB, Gln_DB = st.session_state.DB
+
+        if data["Glc"].iloc[-1] + Glc_DB < st.session_state.SP[0]:
+            F_Glc = st.session_state.flows[0]
+        else:
+            F_Glc = 0
+
+        if data["Gln"].iloc[-1] + Glc_DB < st.session_state.SP[1]:
+            F_Gln = st.session_state.flows[1]
+        else:
+            F_Gln = 0
+    else:
+        t0_glc, tn_glc = st.session_state.t_glc
+        if t0_glc <= data["t"].iloc[-1] <= tn_glc:
+            F_Glc = st.session_state.flows[0]
+        else:
+            F_Glc = 0
+
+        t0_gln, tn_gln = st.session_state.t_gln
+        if t0_gln <= data["t"].iloc[-1] <= tn_gln:
+            F_Gln = st.session_state.flows[1]
+        else:
+            F_Gln = 0
+
+    # Start Calculation
     try:
         res_n = next(
             gen(
--- gui/sidebar.py
+++ gui/sidebar.py
@@ -2,6 +2,9 @@ import streamlit as st
 import numpy as np
 from pandas import DataFrame
 
+BOLUS_TIMES = [1.0, 25.0, 50.0, 100.0]
+DEADBAND = 0.5
+
 if "data" not in st.session_state:
     simulation_inited = False
 else:
@@ -161,25 +164,117 @@ def mk_sidebar(title="Default Title", caption="Default Caption"):
             disabled=st.session_state.auto_refresh,
         )
 
+        control_scheme = st.radio(
+            "Select a Control Scheme",
+            ["Bolus Feed", "Continuous Feed", "Bang Control"],
+            index=1,
+        )
+
         st.subheader("**Feed Flowrates**")
-        col3, col4 = st.columns(2)
-        with col3:
-            F_Glc = st.number_input(
-                "$F_{Glc}$ (L/h)", value=0.0, min_value=0.0, max_value=5.0
-            )
-        with col4:
-            F_Gln = st.number_input(
-                "$F_{Gln}$ (L/h)", value=0.0, min_value=0.0, max_value=5.0
+        if control_scheme == "Bang Control":
+
+            col3, col4 = st.columns(2)
+            with col3:
+                Glc_SP = st.number_input(
+                    "Glc Setpoint (mM)", value=20.0, min_value=0.0, max_value=60.0
+                )
+                Glc_DB = st.number_input(
+                    "Glc Deadband", value=DEADBAND, min_value=0.0, max_value=15.0
+                )
+                F_Glc = st.number_input(
+                    "$F_{Glc}$ (L/h)", value=1.0, min_value=0.0, max_value=2.0
+                )
+            with col4:
+                Gln_SP = st.number_input(
+                    "Gln Setpoint (mM)", value=0.0, min_value=0.0, max_value=15.0
+                )
+                Gln_DB = st.number_input(
+                    "Gln Deadband", value=DEADBAND, min_value=0.0, max_value=15.0
+                )
+                F_Gln = st.number_input(
+                    "$F_{Gln}$ (L/h)", value=1.0, min_value=0.0, max_value=2.0
+                )
+
+            st.session_state.SP = [Glc_SP, Gln_SP]
+            st.session_state.DB = [Glc_DB, Gln_DB]
+            t0_glc, tn_glc = (0.0, 0.0)
+            t0_gln, tn_gln = (0.0, 0.0)
+
+        elif control_scheme == "Bolus Feed":
+
+            options = ["{:0} h".format(int(time)) for time in BOLUS_TIMES[:4]]
+
+            selection = st.segmented_control(
+                "Select Feed Time",
+                options,
+                selection_mode="multi",
             )
 
-        st.subheader("**Dosing Times**")
-        t0_glc, tn_glc = st.select_slider(
-            "Glucose Dosing Time (h)", options=np.arange(0, 251, 25), value=(175, 225)
-        )
+            def isSelected(i):
+                if options[i] not in selection:
+                    return True
+                else:
+                    return False
 
-        t0_gln, tn_gln = st.select_slider(
-            "Glutamine Dosing Time (h)", options=np.arange(0, 251, 25), value=(175, 225)
-        )
+            col3, col4 = st.columns(2)
+            with col3:
+                V_Glc_0 = st.number_input(
+                    f"$V_F,{options[0]}$ (mL)",
+                    value=0.0,
+                    min_value=0.0,
+                    max_value=1500.0,
+                    disabled=isSelected(0),
+                )
+                V_Glc_1 = st.number_input(
+                    f"$V_t,{options[1]}$ (mL)",
+                    value=0.0,
+                    min_value=0.0,
+                    max_value=1500.0,
+                    disabled=isSelected(1),
+                )
+            with col4:
+                V_Glc_2 = st.number_input(
+                    f"$V_t,{options[2]}$ (mL)",
+                    value=0.0,
+                    min_value=0.0,
+                    max_value=1500.0,
+                    disabled=isSelected(2),
+                )
+                V_Glc_3 = st.number_input(
+                    f"$V_t,{options[3]}$ (mL)",
+                    value=0.0,
+                    min_value=0.0,
+                    max_value=1500.0,
+                    disabled=isSelected(3),
+                )
+
+            F_Glc, F_Gln = (0.0, 0.0)
+            t0_glc, tn_glc = (0.0, 0.0)
+            t0_gln, tn_gln = (0.0, 0.0)
+            st.session_state.bolus_feeds = [V_Glc_0, V_Glc_1, V_Glc_2, V_Glc_3]
+        else:
+            col3, col4 = st.columns(2)
+            with col3:
+                F_Glc = st.number_input(
+                    "$F_{Glc}$ (L/h)", value=0.0, min_value=0.0, max_value=5.0
+                )
+            with col4:
+                F_Gln = st.number_input(
+                    "$F_{Gln}$ (L/h)", value=0.0, min_value=0.0, max_value=5.0
+                )
+
+            st.subheader("**Dosing Times**")
+            t0_glc, tn_glc = st.select_slider(
+                "Glucose Dosing Time (h)",
+                options=np.arange(0, 251, 25),
+                value=(175, 225),
+            )
+
+            t0_gln, tn_gln = st.select_slider(
+                "Glutamine Dosing Time (h)",
+                options=np.arange(0, 251, 25),
+                value=(175, 225),
+            )
 
         st.subheader("**Alarms**")
         col5, col6 = st.columns(2)
@@ -191,6 +286,7 @@ def mk_sidebar(title="Default Title", caption="Default Caption"):
             "Max Gln (mM)", value=10.0, min_value=0.0, max_value=100.0
         )
 
+        st.session_state.control_mode = control_scheme
         st.session_state.initial_vals = [Glc_0, Gln_0, X_0, P_0, Amm, Lac, V]
         st.session_state.flows = [F_Glc, F_Gln]
         st.session_state.t_glc = (t0_glc, tn_glc)
